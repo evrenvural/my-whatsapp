f03b068ce18a66580852376335daec679422cc58
evrenvural <evrenvural4@gmail.com>
2019-08-12 23:04:34 +0300

    Updates

---
+++ precache-manifest.d51ee85560e3d3ac96f88c7314d08664.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dac78f8180e47cb991844b217b8ca462",
+    "url": "/my-whatsapp/index.html"
+  },
+  {
+    "revision": "66362a469297d17e1b1f",
+    "url": "/my-whatsapp/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "78ec779670fb7f77c83c",
+    "url": "/my-whatsapp/static/css/main.c0e72b19.chunk.css"
+  },
+  {
+    "revision": "66362a469297d17e1b1f",
+    "url": "/my-whatsapp/static/js/2.f7b716f9.chunk.js"
+  },
+  {
+    "revision": "78ec779670fb7f77c83c",
+    "url": "/my-whatsapp/static/js/main.5aad901a.chunk.js"
+  },
+  {
+    "revision": "77d6194bebc73c5f1b4b",
+    "url": "/my-whatsapp/static/js/runtime~main.5d3ae0b3.js"
+  }
+]);
